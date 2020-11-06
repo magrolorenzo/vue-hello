ae1b47ca97f748e0f8b3e2fd08b14f7bce7df31b
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-06 15:22:48 +0100

    Es ok

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+//  06-11-2020
+var app = new Vue({
+    el: "#root",
+
+    data :{
+        // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+        message: "Buon Weekend!",
+        // BONUS: Aggiungere un’immagine presa anch’essa da un data
+        img_url: "https://borsaefinanza.it/wp-content/uploads/2020/02/space-economy-563x353.jpg",
+    }
+
+});
